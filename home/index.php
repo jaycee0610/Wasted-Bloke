ef7f9231c1427ecc52725d62b778deae0175100e
John Christian Natividad <120337618+jaycee0610@users.noreply.github.com>
2024-05-17 12:14:12 +0800

    Last Send

--- home/index.php
+++ home/index.php
@@ -7,7 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
@@ -18,10 +20,10 @@
 
 
 
-    <div class="container animate__animated animate__fadeIn shadow-lg mt-2 p-3 mb-5">
+    <div class="container animate__animated animate__fadeIn shadow-lg mt-2 p-3 mb-5 mt-5">
 
 
-    
+
         <div class="row" style="">
             <div class="col-8  p-2">
                 <p class="headline2 float-start">THE NEGLECTED NEWS EDITION 2024</p>
@@ -52,12 +54,12 @@
                     let's work together to create a better future for all. Together, we can make a difference.</p>
                 <img class="img-fluid" src="assets/images/1.jpg" alt="">
             </div>
-            <div class="col-sm-3 border-start border-dark   ">
-                <i class="fw-bold p-2">ABOUT WASTED BLOKE</i>
+            <div class="col-sm-3 border-start border-dark pt-3">
+                <i class="fw-bold p-2 ">ABOUT WASTED BLOKE</i>
                 <img class="img-fluid d-none d-md-block" src="assets/images/2.jpg" alt="">
-                <h3 class="headline2 mt-2">IT’S A SOCIAL PROBLEM</h3>
+                <h3 class="headline2 mt-3">IT’S A SOCIAL PROBLEM</h3>
                 <div class="text-center mb-3 mt-3">
-                    <p class="border-top pt-2 border-dark border-bottom pb-2 w-50 d-inline">NOT JUST A MEME</p>
+                    <p class="border-top pt-2 mb-3 border-dark border-bottom pb-2 w-50 d-inline">NOT JUST A MEME</p>
                 </div>
                 <p>"Welcome to WASTED Bloke's corner of the internet, where we tackle life's serious issues with a smile
                     and a side of sarcasm!</p>
@@ -72,7 +74,7 @@
                     meme at a time, let's show the world that laughter and empathy are powerful tools for positive
                     transformation.</p>
             </div>
-            <div class="col-sm-3  border-bottom border-dark">
+            <div class="col-sm-3  border-bottom border-dark pt-3">
                 <h3 class="text-center headline3">OUR VISION AND MISSION </h3>
                 <p class="header-about">VISION</p>
                 <p>To create a world where laughter is a catalyst for positive change, fostering
@@ -86,7 +88,7 @@
 
         <div class="row border-bottom border-dark pb-4 mb-3  mt-3">
 
-            <div class="col-sm-3">
+            <div class="col-sm-3 pt-3">
                 <div class=" border border-dark border-1 p-2">
                     <div class="mt-2">
                         <i class="">EDITION 2024</i>
@@ -214,25 +216,28 @@
                 <div class="container">
                     <!-- First row with three logos -->
                     <div class="row g-3"> <!-- g-3 class for spacing between columns -->
-                        <div class="col-md-4 d-flex justify-content-center align-items-center border">
+                        <div class="col-6 col-md-3 d-flex justify-content-center align-items-center border">
                             <img src="assets/images/pinksale.jpg" alt="PinkSale Logo" class="img-fluid">
                         </div>
-                        <div class="col-md-4 d-flex justify-content-center align-items-center border">
+                        <div class="col-6 col-md-3 d-flex justify-content-center align-items-center border">
                             <img src="assets/images/raydium.jpg" alt="Raydium Logo" class="img-fluid">
                         </div>
-                        <div class="col-md-4 d-flex justify-content-center align-items-center border">
+                        <div class="col-6 col-md-3 d-flex justify-content-center align-items-center border">
                             <img src="assets/images/dextools.jpg" alt="DEXTools Logo" class="img-fluid">
                         </div>
+                        <div class="col-6 col-md-3 d-flex justify-content-center align-items-center border">
+                            <img src="assets/images/dexview.jpg" alt="DEXView Logo" class="img-fluid">
+                        </div>
+
                     </div>
 
                     <!-- Second row with two logos and text -->
                     <div class="row g-3">
-                        <div class="col-md-3 d-flex justify-content-center align-items-center border">
-                            <img src="assets/images/dexview.jpg" alt="DEXView Logo" class="img-fluid">
+                        <div class="col-12 col-md-3 d-flex justify-content-center align-items-center mb-0">
+                            <p class="text-center mb-3 mt-3">AUDITED AND SMART CONTRACT BY:</p>
                         </div>
-                        <div class="col-md-9 border">
+                        <div class="col-12 col-md-9 ">
                             <div class="h-100 d-flex flex-column justify-content-center">
-                                <p class="text-center mb-3 mt-3">AUDITED AND SMART CONTRACT BY:</p>
                                 <div class="d-flex justify-content-center p-3">
                                     <img src="assets/images/coinsult.jpg" alt="Coinsult Logo" class="img-fluid">
                                 </div>
@@ -248,10 +253,10 @@
 
 
                     <div class="row">
-                        <div class="col-6">
-                            <img src="assets/images/book.jpg" alt="" class="img-fluid">
+                        <div class="col-sm-6">
+                            <img src="assets/images/book.jpg" alt="" class="img-fluid mb-3">
                         </div>
-                        <div class="col-6">
+                        <div class="col-sm-6">
                             <p>Helped the client to rearrange the digital
                                 content to get better engagement and traffic
                                 in their social media account.</p>
@@ -284,9 +289,10 @@
         <div class="row mb-3 mt-5">
             <div class="col-12">
                 <div class="text-center">
-                    <h3 class="h13" style="font-size:50px;margin:0;padding:0;">JOIN OUR PRESALE
+                    <h3 class="h13 d-block" style="font-size:50px;margin:0;padding:0;">JOIN OUR PRESALE
                     </h3>
-                    <p>Copyright 2024 WastedBloke
+                    <a class="btn btn-dark  ">Join Now</a>
+                    <p class="mt-3">Copyright 2024 WastedBloke
                         All Rights Reserved</p>
 
                 </div>
@@ -294,7 +300,7 @@
             </div>
         </div>
 
-        <div class="row border-top border-dark" style="">
+        <div class="row border-top border-dark mt-5" style="">
             <div class="col-8  p-2">
                 <p class="headline2 float-start">THE NEGLECTED NEWS EDITION 2024</p>
             </div>
@@ -312,6 +318,37 @@
 
     </div>
 
+
+
+
+    <div class="modal fade" id="onload" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog  modal-dialog-centered">
+            <div class="modal-content modal-default">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Disclaimer 🚨 </h5>
+                </div>
+                <div class="modal-body">
+                This pop-up warning is for informational purposes only and does not constitute financial advice. Always conduct your own research and consult with a qualified financial advisor before making investment decisions.Before you dive in, consider its impact on societal challenges like poverty, homelessness, and alcoholism. While it offers opportunities, it also poses risks, especially for vulnerable communities. Prioritize understanding the market, protect against scams, and never invest more than you can afford to lose. Stay informed, cautious, and seek advice to invest responsibly.
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div> 
+
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+    <script type="text/javascript">
+        window.onload = () => {
+            $('#onload').modal('show');
+        }
+    </script>
 
 
 </body>
